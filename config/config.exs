f2df8b939f3ca065a925f6be6345b19bdd609909
llxff <ll.wg.bin@gmail.com>
2016-10-19 22:44:10 +0300

    add debug

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :logger, compile_time_purge_level: :info
+
 config :issues, github_url: "https://api.github.com"
 config :issues, columns: [
   %{ name: "#", size: 11, value: "id" },
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,9 +1,13 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-Agent", "ll.wg.bin@gmail.com"}]
 
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching #{ user }/#{ project }"
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -14,10 +18,15 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body} }) do
+    Logger.info "Success response"
+    Logger.debug fn -> inspect(body) end
+
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _, %{status_code: _, body: body} }) do
+  def handle_response({ _, %{status_code: status, body: body} }) do
+    Logger.error "Error #{ status } code returned."
+
     { :error, Poison.Parser.parse!(body) }
   end
 end
